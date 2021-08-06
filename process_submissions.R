2d636e888096664922b04899c3fcd0cee386deb1
rqthomas <rqthomas@vt.edu>
2021-08-06 17:14:15 +0000

    Changes from server

--- process_submissions.R
+++ process_submissions.R
@@ -5,9 +5,9 @@ Sys.setenv("AWS_DEFAULT_REGION" = "data",
 
 submissions_directory <- "/efi_neon_challenge/submissions"
 
-source("forecast_output_validator.R")
+#source("forecast_output_validator.R")
 
-object <- aws.s3::get_bucket("submissions")
+object <- aws.s3::get_bucket("submissions", max = Inf)
 
 themes <- c("aquatics", "beetles", "phenology", "terrestrial", "ticks")
 if(length(object) > 0){
@@ -15,18 +15,20 @@ if(length(object) > 0){
     theme <-  stringr::str_split(object[[i]]$Key, "-")[[1]][1]
     theme <- stringr::str_split(theme, "_")[[1]][1]
     submission_date <- lubridate::as_date(paste(stringr::str_split(object[[i]]$Key, "-")[[1]][2:4], collapse = "-"))
-
+    print(i)
     print(object[[i]]$Key)
     print(theme)
+    subdirectory_present <- stringr::str_detect(object[[i]]$Key, "/")
     
-    if(tools::file_ext(object[[i]]$Key) != "log" & !stringr::str_detect(object[[i]]$Key, "processed")){
+    
+    if(!stringr::str_detect(object[[i]]$Key, "processed") & (tools::file_ext(object[[i]]$Key) %in% c("nc", "gz", "csv", "xml")) & !is.na(submission_date) & !subdirectory_present){
       
       log_file <- paste0(submissions_directory,"/",object[[i]]$Key,".log")
       
       if(theme %in% themes & submission_date <= Sys.Date()){
         
         capture.output({
-          valid <- tryCatch(forecast_output_validator(file.path(submissions_directory,object[[i]]$Key)),error = function(e) FALSE, finally = NULL)
+          valid <- tryCatch(neon4cast::forecast_output_validator(file.path(submissions_directory,object[[i]]$Key)),error = function(e) FALSE, finally = NULL)
         }, file = log_file, type = c("message"))
         
         if(valid){
